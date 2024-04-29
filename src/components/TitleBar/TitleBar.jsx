3618ee9a870f3ad4e217d7e87a62c806a81279cd
SHUBHAM-126 <shubhammhatre126@gmail.com>
2024-04-29 23:55:42 +0530

    Added static titleBar

--- src/components/TitleBar/TitleBar.jsx
+++ src/components/TitleBar/TitleBar.jsx
@@ -1,9 +1,30 @@
 import styles from './TitleBar.module.css'
+import { FaRegStar } from "react-icons/fa";
+import { MdOutlinePeopleOutline } from "react-icons/md";
+import { RiShareBoxLine } from "react-icons/ri";
+import { HiDotsHorizontal } from "react-icons/hi";
 
-export default function TitleBar(){
+export default function TitleBar() {
     return (
-        <div>
-            Title bar
+        <div className={styles.titlebar}>
+            <div className={styles.innerWrapper}>
+                <h2 className={styles.title}>My Trello Board</h2>
+                <button className={styles.iconButton}>
+                    <FaRegStar />
+                </button>
+                <button className={styles.iconButton}>
+                    <MdOutlinePeopleOutline />
+                </button>
+            </div>
+            <div className={styles.innerWrapper}>
+                <button className={styles.shareButton}>
+                    <span>Share</span>
+                    <RiShareBoxLine />
+                </button>
+                <button className={styles.iconButton}>
+                    <HiDotsHorizontal />
+                </button>
+            </div>
         </div>
     )
 }
